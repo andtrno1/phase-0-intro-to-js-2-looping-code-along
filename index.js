52c41601d8663b176959f956de02bba91807c2ab
andtrno1 <andtrno1@gmail.ccom>
2022-03-16 18:43:57 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function countDown(number){
+    for (var number = 0; number < 11; number++){
+        console.log(number);
+    }
+}
+
+
+
+
+function writeCards(Name, event){
+    const message = [];
+    for (let i=0; i<Name.length; i++){
+        message.push(`Thank you, ${Name[[i]]}, for the wonderful ${event} gift!`)
+        }
+    return message;
+
+}
+
+
+
